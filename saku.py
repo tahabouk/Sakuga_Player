ecb39969add812ad8047637a7517cea11ec92dcc
tahabouk <tahabouk1999@gmail.com>
2026-02-03 19:54:39 +0100

    just copied it and changed somethings

---
+++ saku.py
@@ -0,0 +1,59 @@
+import subprocess
+import sys
+import time
+import json
+import random
+
+
+def play():
+    global mpv
+
+    width = 854
+    height =480
+    
+
+    mpv = subprocess.Popen(
+        ["mpv","--title='Sakuga Clip' ","--prefetch-playlist=yes" ,f"--geometry={width}x{height}", f"--playlist={playlist}"],
+        stdout=subprocess.DEVNULL,  
+        stderr=subprocess.DEVNULL 
+        )
+
+
+
+def main():
+    global playlist
+
+    file = "info.jsonl"
+
+    playlist = "playlist.txt"
+
+    data = []
+
+    size = 250
+
+    with open(file , "r") as f :
+
+        for line in f :
+
+            data.append(json.loads(line.strip()))
+
+    with open(playlist,"w") as f :
+
+        samples = random.sample(data,size)
+        
+
+        for sample in samples :
+
+            link = sample["link"]
+
+            f.write(link + "\n")
+
+    play()
+
+
+if __name__ == "__main__":
+    try :
+        main()
+    except :
+        mpv.terminate()
+
---
+++ saku_links_get.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def read_write(gharad,file,info=None):
+
+    if gharad == "write" :
+
+        with open(file , "a") as f :
+            f.write(json.dumps(info) + "\n")
+
+    elif gharad == "read" :
+
+        with open(file , "r") as f :
+
+            for line in f :
+
+                data = json.loads(line.strip())
+
+                print(data)
+
+
+
+
+
+def parse(rep):
+
+    file = "info.jsonl"
+
+    hit = 0
+
+    soup = BeautifulSoup(rep.text,"xml")
+
+    posts = soup.find_all("post")
+
+    for post in posts :
+
+        url = post.get("file_url")
+
+        score = post.get("score") ; score = int(score)
+
+        size = post.get("file_size") ; size = int(size)
+
+        tag = post.get("tags")
+
+        image = post.get("preview_url")
+
+        if score > 300 and size > 5000000 and ".mp4" in url:
+
+            hit +=1
+
+            video = {"link" : url , "score" : score ,} 
+
+            with open(file , "a") as f :
+
+                f.write(json.dumps(video) + "\n")
+
+    print(f"{hit} good videos")
+
+
+
+
+def main() :
+
+
+
+    for page in range(1,1764) :
+
+        base_url = f"https://www.sakugabooru.com/post.xml?page={page}&limit=100"
+
+        rep = requests.get(base_url)
+
+        liest = parse(rep)
+
+        print(base_url)
+
+
+main()
